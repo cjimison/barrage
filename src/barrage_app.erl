2f46d764dd0d13c311b6abf0989e8a1c8409cddc
Chris Jimison <cjimison@ngmoco.com>
2013-04-19 14:28:39 -0700

    General cleanup and moving towards distributing the commands.  This will need to be done on a feature branch but I am also hoping this will allow me to change the action and behavior files on the fly which would be way cooler….

--- src/barrage_app.erl
+++ src/barrage_app.erl
@@ -109,140 +109,6 @@ stop(_State) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%%  Process all the behaviors and load them into an ets table.
-%%  int he future I would like to make this modifiable
-%%  but then I will need to make it work in a cluster....
-%%
-%% @spec process_plans(Plans) -> ok
-%%      Plans = Array
-%% @end
-%%--------------------------------------------------------------------
-process_plans([]) ->
-    ok;
-
-process_plans(Plans) when true =:= is_list(Plans) ->
-    [ThePlan | OtherPlans]  = Plans,
-    case is_tuple(ThePlan) of
-        true ->
-            {Plan}  = ThePlan,
-            Name    = proplists:get_value(<<"name">>, Plan),
-            Tree    = proplists:get_value(<<"tree">>, Plan),
-            
-            case (Name /= undefined andalso Tree /= undefined) of
-                true ->
-                    [{table_keys, OldKeys}] = ets:lookup(plans, table_keys),
-                    NewKeys = [Name | OldKeys],
-                    ets:insert(plans, {table_keys, NewKeys}),
-                    ets:insert(plans, {Name, Tree}),
-                    process_plans(OtherPlans);
-                false ->
-                    invalid_plan_elements
-            end;
-        false ->
-            invalid_plan
-    end;
-
-process_plans(_Plans) ->
-    invalid_plans.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%  Process the Actions defined by the user
-%%
-%% @spec process_actions(Actions) -> ok
-%%      Plans = Array
-%% @end
-%%--------------------------------------------------------------------
-process_actions([]) ->
-    ok;
-
-process_actions(Actions) when true =:= is_list(Actions) ->
-    [TheAction | OtherActions]  = Actions,
-    case is_tuple(TheAction) of
-        true ->
-            {Action}= TheAction,
-            Name    = proplists:get_value(<<"name">>, Action),
-            case (Name /= undefined) of
-                true ->
-                    ets:insert(actions, {Name, Action}),
-                    process_actions(OtherActions);
-                false ->
-                    invalid_action_element
-            end;
-        false ->
-            invalid_action
-    end;
-
-process_actions(_Actions) ->
-    invalid_actions.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%  Process the Config data
-%%
-%% @spec process_actions(Actions) -> ok
-%%      Plans = Array
-%% @end
-%%--------------------------------------------------------------------
-process_config([]) ->
-    ok;
-
-process_config(Configs) when true =:= is_list(Configs) ->
-    [Config | OtherConfigs] = Configs,
-    case process_config(Config) of
-        ok ->
-            process_config(OtherConfigs);
-        Error ->
-            Error
-    end;
-
-process_config(TheConfig) when true =:= is_tuple(TheConfig) ->
-    {Config} = TheConfig,
-    Type     = proplists:get_value(<<"type">>, Config),
-    TheArgs  = proplists:get_value(<<"args">>, Config),
-    case (undefined /= Type andalso undefined /= TheArgs andalso is_tuple(TheArgs)) of
-        true ->
-            {Args} = TheArgs,
-            ets:insert(barrage, {type, Type}),
-            case Type of
-                <<"general">> ->
-                    ets:insert(barrage, {enable_general, true}),
-                    case proplists:get_value(<<"url">>, Args) of
-                        undefined ->
-                            invalid_config_arg;
-                        URL ->
-                            ets:insert(barrage, {url, URL}),
-                            ok
-                    end;
-                <<"commander">> ->
-                    ets:insert(barrage, {enable_commander, true}),
-                    Gunners = proplists:get_value(<<"gunners">>, Args),
-                    GeneralA= proplists:get_value(<<"general">>, Args),
-                    case (Gunners /= undefined andalso GeneralA /= undefined) of
-                        true ->
-                            General = binary_to_atom(GeneralA, utf8),
-                            ets:insert(barrage, {general, General}),
-                            ets:insert(barrage, {gunners, Gunners}),
-                            ok;
-                        false ->
-                            invalid_config_arg
-                    end;
-                _ ->
-                    invalid_config_type
-
-            end;
-        false ->
-            invalid_config
-    end;
-
-process_config(_Configs) ->
-    invalid_config.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
 %%  Loads all the config files that currently exist for the system
 %%
 %% @spec stop(State) -> void()
@@ -259,21 +125,11 @@ loadConfigTable()->
         Configs         = jiffy:decode(ConfigsJ),
         Plans           = jiffy:decode(PlansJ),
         Actions         = jiffy:decode(ActionsJ),
-        
+    
         % Now lets set parse the plans in order to build out an ets table
-        ets:new(barrage, [set, named_table]),
-
-        ets:insert(barrage, {enable_general, false}),
-        ets:insert(barrage, {enable_commander, false}),
-        ok = process_config(Configs),
-
-        ets:new(plans, [set, named_table]),
-        ets:insert(plans, {table_keys, []}),
-        ok = process_plans(Plans),
-
-        ets:new(actions, [set, named_table]),
-        process_actions(Actions)
-
+        ok = barrage_parser:load_sys_data(Configs),
+        ok = barrage_parser:load_behavior_data(Plans),
+        ok = barrage_parser:load_action_data(Actions)
     catch 
         _:_ ->
             error_data
