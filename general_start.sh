222c61440199ca29bf034a4d700fb25bcd646395
Chris Jimison <cjimison@ngmoco.com>
2013-04-23 11:05:12 -0700

    This should fix the issue of distributed load not working…
    Issue number: https://github.com/cjimison/barrage/issues/30

--- general_start.sh
+++ general_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./rebar compile
-erl -name general@localhost -setcookie barrage_cluster erl -pa `pwd`/ebin deps/*/ebin -s barrage -eval "io:format(\"Started~n\")."
+erl -name general@127.0.0.1 -setcookie barrage_cluster -pa `pwd`/ebin deps/*/ebin -s barrage -eval "io:format(\"Started~n\")."
